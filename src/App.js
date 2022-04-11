6e260fff886920ae9a6f11038a4332d4d456a348
Rashad Moore <rjm4god7@gmail.com>
2022-04-11 15:52:55 -0500

    added bg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
-import amalfi from './amalfi.jpg';
-import background from "./bgph.png";
+import bgtest1 from "./images/bgtest1.jpg";
 import './App.css';
 import Navigation from './components/Navbar';
+import Home from './components/home';
 
 
 function App() {
   return (
-    <div className="App" style={{backgroundImage: `url(${background})`}}>
+    <div className="app-landing" style={{backgroundImage: `url(${bgtest1})` }}>
 <Navigation/>
+<Home/>
 
     </div>
   );
