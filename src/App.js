8ae266d67a4674704eeff21289a70b3703480491
Rashad Moore <rjm4god7@gmail.com>
2022-04-11 09:52:22 -0500

    created Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import {Navbar, Nav} from 'react-bootstrap'
+import logoph from './logoph.png'
 
 function App() {
   return (
-    <div className="App">
-     <p>
-          Let's Travel!!!!!
-     </p>
-        </div>
+    <div className = "App">
+<Navbar bg='myGray' sticky="top" expand="md"> 
+<Navbar.Brand className = "myNav">
+<img src={logoph} className ="myLogo" width= "250px" height="250px"/>
+</Navbar.Brand>
+
+<Navbar.Toggle/>
+<Navbar.Collapse>
+<Nav>
+  <Nav.Link href="about-us">About Us</Nav.Link>
+  <Nav.Link href="contact-us">Contact Us</Nav.Link>
+</Nav>
+</Navbar.Collapse>
+
+</Navbar>
+
+    </div>
   );
 }
 
