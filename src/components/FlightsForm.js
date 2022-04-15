0831df3d18ed04d19ea6871f06c7a34515841e64
Kim Carmona <kimsams@hotmail.com>
2022-04-15 14:13:56 -0500

    begin flight search form

--- src/components/FlightsForm.js
+++ src/components/FlightsForm.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
+import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Control, Form, Errors } from 'react-redux-form';
+
+export const dateValidator = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
 
 class Flights extends Component {
     constructor(props) {
@@ -9,7 +12,13 @@ class Flights extends Component {
             depatureDate: '',
             returnDate: '',
             origin: '',
-            destination: ''
+            destination: '',
+            touched: {
+                departureDate: false,
+                returnDate: false,
+                origin: false,
+                destination: false
+            }
         };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,6 +30,47 @@ handleSubmit(values) {
 render () {
 
     return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem>NOT SURE YET KSC</BreadcrumbItem>
+                        <BreadcrumbItem active>Flight Search</BreadcrumbItem>
+                    </Breadcrumb>
+                    <h2>Flight Search</h2>
+                    <hr />
+                </div>
+            </div>
+
+            <div className="col">
+                <Form onSubmit={values => this.handleSubmit(values)} >
+                    <Row className="form-group">
+                        <Label htmlFor="departureDate" md={2}>Departure Date</Label>
+                        <Col>
+                            <Control.text model=".departureDate" id="departureDate" name="departureDate"
+                                placeholder="Departure Date"
+                                className='form-control'
+                                validators={{
+                                    required,
+                                    dateValidator,
+                                }}
+                            />
+                            <Errors
+                                className="text-danger"
+                                model=".departureDate"
+                                show="touched"
+                                component="div"
+                                messages={{
+                                    required: 'Required',
+                                    dateValidator: 'Must use YYYY-MM-DD format'
+                                }}
+                            />
+                        </Col>
+                    </Row>
+                </Form>
+            </div>
+
+        </div>
 
     );
 }
