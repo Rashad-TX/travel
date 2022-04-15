02af268880e0c05d184f755e94a0a6dc9068a4b9
Kim Carmona <kimsams@hotmail.com>
2022-04-15 12:29:33 -0500

    flight api to console

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Navbar, Nav} from 'react-bootstrap';
+import getFlightsData from './api/Flights';
 import bgtest1 from "./images/bgtest1.jpg";
 import './App.css';
 import Navigation from './components/Navbar';
 import Home from './components/home';
 
 
-function App() {
+const App = () => {
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    getFlightsData()
+      .then((response) => {
+        console.log(response);
+        setFlights(response);
+      });
+  }, []);
+
   return (
     <div className="app-landing" style={{backgroundImage: `url(${bgtest1})` }}>
 <Navigation/>
