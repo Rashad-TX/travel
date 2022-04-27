56f3b764efb3c95b537a3e15db380e2d843c1ca0
Rashad Moore <rjm4god7@gmail.com>
2022-04-27 15:37:07 -0500

    added opentripmap api

--- src/components/home.js
+++ src/components/home.js
@@ -2,30 +2,34 @@ import React from "react";
 import {Button} from "react-bootstrap"
 import axios from 'axios';
 
+const options = {
+  method: 'GET',
+  url: 'https://opentripmap-places-v1.p.rapidapi.com/%7Blang%7D/places/xid/%7Bxid%7D',
+  headers: {
+    'X-RapidAPI-Host': 'opentripmap-places-v1.p.rapidapi.com',
+    'X-RapidAPI-Key': 'faa3a32a1bmsh74d15fe0a7dc28bp1902b5jsn8da2d3d8f568'
+  }
+};
 
+const getRandomCity = async () => {
+  try {
+      const response = await axios.get(URL, options); //request data
 
+      return response;
+  } catch (error) {
+      console.log(error)
+  }
+}
 
 function Home() {
- const[location, setLocation] = useState("");
-
-    const getRandomCity = async () => {
-         const response = await axios.get("https://api.roadgoat.com/api/v2/destinations/:id").then(response) => {
-          setLocation(response.data.name);   
-         };
-         
-         }
-        console.log(response.data);
-        const data = await Response.data
-               
-      };
-
+  
     return(
         <div className ="home-body">
             <Button className ="my-btn" onClick={getRandomCity}>Let's Go!</Button>
       
         <br></br>
         <div className="photo">
-        {location}
+   
         
        </div>
     </div>
